b57d204f9ed2b766a07a7060407d62410d5f85b8
Anastasia1924 <Anastasiastasia82@gmail.com>
2025-02-02 19:20:28 +0400

    дипломная работа

--- pages/ApiClass.py
+++ pages/ApiClass.py
@@ -15,7 +15,7 @@ class ApiClass:
         data = DataForTests()
         url = data.base_url_api + "films"
         resp = requests.get(url, headers=self.headers)
-        body = resp.json()  # Исправлено на вызов метода .json()
+        body = resp.json()  #вызов метода .json()
         return body
 
     @allure.step("Поиск фильма по ID")
@@ -23,4 +23,21 @@ class ApiClass:
         data = DataForTests()
         url = data.base_url_api + "films" + data.kinopoisk_id
         resp = requests.get(url, headers=self.headers)
+        body = resp.json()  # вызов метода .json()
+        return body
+
+    @allure.step("Поиск фильма по названию")
+    def get_film_by_name(self):
+        data = DataForTests()
+        url = data.base_url_api + "films" + data.name_ru
+        resp = requests.get(url, headers=self.headers)
         return resp
+
+    @allure.step("Получение данных о наградах сериала 'Друзья'")
+    def get_data_awards_by_friends(self):
+        data = DataForTests()
+        url = data.base_url_api + "films" + data.friend_id + "awards"
+        resp = requests.get(url, headers=self.headers)
+        body = resp.json()  # вызов метода .json()
+        return body
+
--- tests/test_api.py
+++ tests/test_api.py
@@ -15,6 +15,7 @@ def test_get_list():
     body = api.get_all_films()
     assert len(body) > 0
 
+
 @allure.epic("API тесты")
 @allure.title("Получение фильма по ID")
 @allure.feature("Позитивные проверки")
@@ -22,4 +23,29 @@ def test_get_list():
 @allure.severity("Critical")
 def test_get_film_by_id():
     api = ApiClass()
-    api.get_all_films()
+    api.get_film_by_id()
+
+    # проверки не знаю как сделать
+
+
+@allure.epic("API тесты")
+@allure.title("Получение фильма по ID")
+@allure.feature("Позитивные проверки")
+@allure.description("В результате проверяется...")
+@allure.severity("Critical")
+def test_get_film_by_name():
+    api = ApiClass()
+    api.get_film_by_name()
+
+    # проверки не знаю как сделать
+
+@allure.epic("API тесты")
+@allure.title("Получение данных о наградах сериала 'Друзья'")
+@allure.feature("Позитивные проверки")
+@allure.description("В результате проверяется...")
+@allure.severity("Critical")
+def test_get_data_awards_by_friends():
+    api = ApiClass()
+    body = api.get_data_awards_by_friends()
+    assert len(body) > 0
+
