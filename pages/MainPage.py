fdd853b9f0a69ca4c47fff25109d7732d4342bd5
Anastasia1924 <Anastasiastasia82@gmail.com>
2025-02-02 14:37:46 +0400

    дипломная работа

--- pages/MainPage.py
+++ pages/MainPage.py
@@ -2,8 +2,7 @@ import allure
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.remote.webelement import WebElement
-from selenium.common.exceptions import TimeoutException
+
 
 @allure.epic("UI тесты")
 class MainPage:
@@ -13,26 +12,68 @@ class MainPage:
         self._driver.maximize_window()
         self.wait = WebDriverWait(driver, 10)
 
-    @allure.step("Открытие формы(переход на страницу)")
-    def open_form(self):
-        self._driver.get("https://www.kinopoisk.ru")
+    @allure.step("Открыть заданную страницу")
+    def open_page(self, url):
+        self._driver.get(url)
 
     @allure.step("Ввод фразы в поиск")
     def enter_search_name(self, name: str):
-        search_input = self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "input[name=kp_query]")))
+        search_input = self.wait.until(EC.visibility_of_element_located((
+            By.CSS_SELECTOR, "input[name=kp_query]")))
         search_input.clear()
         search_input.send_keys(name)
 
-    @allure.step("Клик на 'лупу' в строке поиска")
+    @allure.step("Подтверждение поиска")
     def submit_search(self):
-        search_button = self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[tipe=submit]")))
+        search_button = self.wait.until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type=submit]")))
         search_button.click()
 
     @allure.step("Получение списка фильмов по предварительному поиску")
     def get_search_preview_results(self):
-        self.wait.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "h4.kinopoisk-header-suggest-item__title a")))
-        film_links_titles = self._driver.find_elements(By.CSS_SELECTOR, "h4.kinopoisk-header-suggest-item__title a")
+        self.wait.until(
+            EC.visibility_of_all_elements_located((
+                By.CSS_SELECTOR, "h4.kinopoisk-header-suggest-item__title a")))
+        film_links_titles = self._driver.find_elements(
+            By.CSS_SELECTOR, "h4.kinopoisk-header-suggest-item__title a")
         return [film_title.text for film_title in film_links_titles]
+
+    @allure.step("Выбор фильма")
+    def select_film(self, film_id):
+        film_by_id = self.wait.until(
+            EC.element_to_be_clickable((By.ID, f"suggest-item-film-{film_id}")))
+        film_by_id.click()
+
+    @allure.step("Клип по кнопке 'Оценить фильм'")
+    def click_rate_film(self):
+        rate_button = self.wait.until(
+            EC.element_to_be_clickable((By.XPATH, "//button[text()='Оценить фильм']")))
+        rate_button.click()
+
+    @allure.step("Клип по кнопке 'Оценить фильм'")
+    def rate_film(self, rating):
+        button = self.wait.until(
+            EC.element_to_be_clickable((By.XPATH, f"//button[contains(@aria-label, 'Оценка {rating}')]")))
+        button.click()
+
+    @allure.step("Получение рейтинга")
+    def get_rating(self):
+        element = self.wait.until(
+            EC.visibility_of_element_located((By.XPATH, "//span[@data-tid='5cb64c50']"))
+        )
+        return element.text
+
+    @allure.step("Нажатие на кнопку 'Буду смотреть'")
+    def click_watch_button(self):
+        button = self.wait.until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, "button[title='Буду смотреть']"))
+        )
+        button.click()
+
+    @allure.step("Просмотр добавленного фильма в папку 'Буду смотреть'")
+    def added_film_to_folder(self):
+        folder_element = self.wait.until(EC.visibility_of_element_located((By.ID, "film_47984")))
+        return folder_element.is_displayed(), "Элемент не появился на экране."
 
     @allure.step("Поиск случайного фильма по жанру")
     def search_by_genre_random(self, genre: str):
@@ -48,13 +89,6 @@ class MainPage:
             EC.presence_of_element_located((By.CSS_SELECTOR, "div.filmName a"))
         )
         return film_title_element.text if film_title_element else None
-
-
-    @allure.step("Оценить фильм")
-    def evaluate_the_film(self):
-        self._driver.find_element(By.CSS_SELECTOR, "a[data-tid='73187f6b']").click()
-        self._driver.find_element(By.CSS_SELECTOR, "button=Оценить фильм").click()
-        self._driver.find_element(By.CSS_SELECTOR, "button[aria-label='Оценка 10']").click()
 
 
 
--- tests/test_ui.py
+++ tests/test_ui.py
@@ -1,7 +1,20 @@
 import allure
 import pytest
-from selenium import webdriver
 from pytest_ui_api_template.pages.MainPage import MainPage
+from pytest_ui_api_template.config import *
+
+
+@allure.epic("UI тесты")
+@allure.title("Название заголовка")
+@allure.feature("Позитивные проверки")
+@allure.description("В результате проверяется название заголовка")
+@allure.severity("Critical")
+def test_browser_title(browser):
+    with allure.step("Переход на главную страницу)"):
+        main_page = MainPage(browser)
+        main_page.open_page(base_url)
+        browser_title = browser.title
+        assert browser_title == "Кинопоиск. Онлайн кинотеатр. Фильмы сериалы мультфильмы и энциклопедия"
 
 
 @allure.epic("UI тесты")
@@ -10,23 +23,21 @@ from pytest_ui_api_template.pages.MainPage import MainPage
 @allure.description("В результате проверяется , что введённое значение есть в названии первого фильма")
 @allure.severity("Critical")
 @pytest.mark.parametrize("film_title", ["Король Лев", "Титаник", "Зеленая миля"])
-def test_positive_search_for_name(film_title):
-    with allure.step("Открытие сайта(переход на главную страницу)"):
-        driver = webdriver.Chrome()
-        main_page = MainPage(driver)
-        main_page.open_form()
+@pytest.mark.positive
+@pytest.mark.smoke
+def test_fast_search_by_film_title(browser, film_title):
+    with allure.step("Переход на главную страницу)"):
+        main_page = MainPage(browser)
+        main_page.open_page(base_url)
 
     with allure.step("Ввод названия фильма в поисковую строку"):
         main_page.enter_search_name(film_title)
 
-    with allure.step("Получить название фильмов"):
+    with allure.step("Получить название фильмы"):
         list_elements = main_page.get_search_preview_results()
 
     with allure.step("Проверить, что введённое значение есть в названии первого фильма"):
-        assert film_title in list_elements
-
-    with allure.step("Закрыть браузер"):
-        driver.quit()
+        assert film_title in list_elements, f"Ожидалось, что фильм {film_title} будет в результатах поиска"
 
 
 @allure.epic("UI тесты")
@@ -35,39 +46,108 @@ def test_positive_search_for_name(film_title):
 @allure.description("В результате проверяется")
 @allure.severity("Critical")
 @pytest.mark.parametrize("film_genre", ["комедия", "аниме", "музыка"])
-def test_positive_search_for_genre(film_genre):
-    with allure.step("Открытие сайта(переход на главную страницу)"):
-        driver = webdriver.Chrome()
-        main_page = MainPage(driver)
-        main_page.open_form()
+@pytest.mark.positive
+@pytest.mark.smoke
+def test_search_for_genre(browser, film_genre):
+    with allure.step("Переход на главную страницу)"):
+        main_page = MainPage(browser)
+        main_page.open_page(base_url)
 
     with allure.step("В поисковой строке нажатие на 'поиск'"):
         main_page.submit_search()
 
-    with allure.step("Поиск случайного фильма по жанру"):
+    with allure.step("В поисковой строке нажатие на 'поиск'"):
         main_page.search_by_genre_random(film_genre)
         film_title = main_page.get_film_title()
         assert film_title, "Название фильма пустое!"
 
-    with allure.step("Закрыть браузер"):
-        driver.quit()
 
-    @allure.epic("UI тесты")
-    @allure.title("Оценить фильм")
-    @allure.feature("Позитивные проверки")
-    @allure.description("В результате проверяется проставленная оценка")
-    @allure.severity("Critical")
-    def test_positive_evaluate_the_film():
-        with allure.step("Открытие сайта(переход на главную страницу)"):
-            driver = webdriver.Chrome()
-            main_page = MainPage(driver)
-            main_page.open_form()
+# Оценить фильм:
+@allure.epic("UI тесты")
+@allure.title("Оценка фильма")
+@allure.feature("Позитивные проверки")
+@allure.description("В результате проверяется")
+@allure.severity("Critical")
+@pytest.mark.parametrize("film_title, film_id, rating", [("Фантомас", 522876, 10)])
+@pytest.mark.positive
+def test_rate_film(browser, film_title, film_id, rating):
+    with allure.step("Переход на главную страницу)"):
+        main_page = MainPage(browser)
+        main_page.open_page(base_url)
 
-        with allure.step("Ввод названия фильма в поисковую строку"):
-            name = "Век Адалин"
-            main_page.enter_search_name(name)
+# 1. Ввести название фильма "Фантомас"
+    with allure.step("Ввод названия фильма в поисковую строку"):
+        main_page.enter_search_name(film_title)
 
-        with allure.step("Выбрать нужный фильм"):
-            main_page.evaluate_the_film()
+# 2. Провалиться в найденный фильм.
+    with allure.step("Клик по первому названию"):
+        main_page.select_film(film_id)
 
+# 3. Нажать кнопку оценить фильм.
+    with allure.step("Нажать на 'Оценить фильм'"):
+        main_page.click_rate_film()
+
+# 4. Выбрать 10
+    with allure.step("Оценить фильм"):
+        main_page.rate_film(rating)
+
+# 5. Проверить, что оценка 10
+    with allure.step("Проверка рейтинга"):
+        rating = main_page.get_rating()
+        assert rating == 10, f"Ожидалось, что рейтинг будет 10, но получено: {rating}"
+
+
+
+# Добавить фильм в "буду смотреть".
+@allure.epic("UI тесты")
+@allure.title("Добавить фильм в ' Буду смотреть'")
+@allure.feature("Позитивные проверки")
+@allure.description("В результате проверяется")
+@allure.severity("Critical")
+@pytest.mark.parametrize("film_title, film_id", [("Фантомас", 47984)])
+@pytest.mark.positive
+def test_rate_film(browser, film_title, film_id):
+    with allure.step("Переход на главную страницу)"):
+        main_page = MainPage(browser)
+        main_page.open_page(base_url)
+
+# 1. Ввести название фильма "Фантомас"
+    with allure.step("Ввод названия фильма в поисковую строку"):
+        main_page.enter_search_name(film_title)
+
+# 2. Провалиться в найденный фильм.
+    with allure.step("Клик по первому названию"):
+        main_page.select_film(film_id)
+
+# 3 Нажать на кнопку буду смотреть.
+    with allure.step("Клик на кнопку '+ Буду смотреть'"):
+        main_page.click_watch_button()
+
+# Проверить добавлен ли фильм в папку
+    with allure.step("Переход на страницу с папками"):
+        main_page.open_page(folder_url)
+        main_page.added_film_to_folder(film_id)
+        assert film_id == "film_47984"
+
+
+@allure.epic("UI тесты")
+@allure.title("Удалить фильм из папки ' Буду смотреть'")
+@allure.feature("Позитивные проверки")
+@allure.description("В результате проверяется")
+@allure.severity("Critical")
+@pytest.mark.parametrize("film_title, film_id", [("Фантомас", 47984)])
+@pytest.mark.positive
+def test_delete_film_from_folder(browser, film_title, film_id):
+    with allure.step("Переход на страницу с папками"):
+        main_page.open_page(folder_url)
+
+
+
+
+
+# Удалить фильм с папки  "буду смотреть ".
+# 1.На своей странице навести на иконку с фото профиля, нажать на "Фильмы".
+# 2. В папке " буду смотреть " поставить галочку (выбрать фильм).
+# 3. Нажать удалить.
+# 4. Проверить, что фильм удалён  или в этой папке нет фильма с таким названием.
 
