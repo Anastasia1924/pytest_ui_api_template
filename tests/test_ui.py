38b0dd12a9ff00eaa48107c92bfc8b0768df1348
Anastasia1924 <Anastasiastasia82@gmail.com>
2025-02-06 01:27:59 +0400

    дипломная работа

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -43,7 +43,7 @@ def test_fast_search_by_film_title(browser, film_title):
 @allure.epic("UI тесты")
 @allure.title("Поиск фильмов по жанру")
 @allure.feature("Позитивные проверки")
-@allure.description("В результате проверяется")
+@allure.description("В результате проверяется название фильма, найденного по жанру")
 @allure.severity("Critical")
 @pytest.mark.parametrize("film_genre", ["комедия", "аниме", "музыка"])
 @pytest.mark.positive
@@ -56,17 +56,16 @@ def test_search_for_genre(browser, film_genre):
     with allure.step("В поисковой строке нажатие на 'поиск'"):
         main_page.submit_search()
 
-    with allure.step("В поисковой строке нажатие на 'поиск'"):
+    with allure.step("Получение названия фильма"):
         main_page.search_by_genre_random(film_genre)
         film_title = main_page.get_film_title()
         assert film_title, "Название фильма пустое!"
 
 
-# Оценить фильм:
 @allure.epic("UI тесты")
 @allure.title("Оценка фильма")
 @allure.feature("Позитивные проверки")
-@allure.description("В результате проверяется")
+@allure.description("В результате проверяется оценка рейтинга")
 @allure.severity("Critical")
 @pytest.mark.parametrize("film_title, film_id, rating", [("Фантомас", 522876, 10)])
 @pytest.mark.positive
@@ -75,33 +74,27 @@ def test_rate_film(browser, film_title, film_id, rating):
         main_page = MainPage(browser)
         main_page.open_page(base_url)
 
-# 1. Ввести название фильма "Фантомас"
     with allure.step("Ввод названия фильма в поисковую строку"):
         main_page.enter_search_name(film_title)
 
-# 2. Провалиться в найденный фильм.
     with allure.step("Клик по первому названию"):
         main_page.select_film(film_id)
 
-# 3. Нажать кнопку оценить фильм.
     with allure.step("Нажать на 'Оценить фильм'"):
         main_page.click_rate_film()
 
-# 4. Выбрать 10
-    with allure.step("Оценить фильм"):
+    with allure.step("Выбрать 10"):
         main_page.rate_film(rating)
 
-# 5. Проверить, что оценка 10
     with allure.step("Проверка рейтинга"):
         rating = main_page.get_rating()
         assert rating == 10, f"Ожидалось, что рейтинг будет 10, но получено: {rating}"
 
 
-# Добавить фильм в "буду смотреть".
 @allure.epic("UI тесты")
-@allure.title("Добавить фильм в ' Буду смотреть'")
+@allure.title("Ввод произвольного набора символов")
 @allure.feature("Позитивные проверки")
-@allure.description("В результате проверяется")
+@allure.description("В результате выходит сообщение, что ничего не найдено")
 @allure.severity("Critical")
 @pytest.mark.parametrize("film_title", ["125jkhyf"])
 @pytest.mark.positive
@@ -110,15 +103,12 @@ def test_empty_search(browser, film_title):
         main_page = MainPage(browser)
         main_page.open_page(base_url)
 
-# Ввести произвольный набор символов
     with allure.step("Ввод названия фильма в поисковую строку"):
         main_page.enter_search_name(film_title)
 
-# Подтвердить поиск"
     with allure.step("Подтверждение поиска"):
         main_page.submit_search()
 
-# Получение сообщения "К сожалению по вашему запросу ничего не найдено"
-    with allure.step("Подтверждение поиска"):
+    with allure.step("Получение сообщения"):
         empty_message = main_page.get_empty_message()
         assert empty_message == "К сожалению, по вашему запросу ничего не найдено..."
